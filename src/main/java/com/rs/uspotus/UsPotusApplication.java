29fa32bb6f0fc963f12ff7a1408bcea1c6ed4fc2
rstorf25 <61019008+rstorf25@users.noreply.github.com>
2024-02-10 14:41:55 -0600

    Initial spring project add.

---
+++ src/main/java/com/rs/uspotus/UsPotusApplication.java
@@ -0,0 +1,13 @@
+package com.rs.uspotus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsPotusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsPotusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rs/uspotus/UsPotusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rs.uspotus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsPotusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
