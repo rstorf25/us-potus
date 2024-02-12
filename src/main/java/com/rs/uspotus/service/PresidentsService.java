dda58d39d033e85a5451e2bce3273ab23ba226d6
rstorf25 <61019008+rstorf25@users.noreply.github.com>
2024-02-12 11:15:23 -0600

    add president delete functionality in controller and service.

--- src/main/java/com/rs/uspotus/service/PresidentsService.java
+++ src/main/java/com/rs/uspotus/service/PresidentsService.java
@@ -4,22 +4,32 @@ import com.rs.uspotus.model.President;
 import com.rs.uspotus.repository.PresidentsRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 
 @Service
 public class PresidentsService {
 
     private final PresidentsRepository presidentsRepository;
 
-    public PresidentsService(PresidentsRepository presidentsRepository){
+    public PresidentsService(PresidentsRepository presidentsRepository) {
         this.presidentsRepository = presidentsRepository;
     }
 
-    public President addPresident(President president){
+    public President addPresident(President president) {
         return presidentsRepository.save(president);
     }
 
-    public Iterable<President> getAllPresidents(){
+    public Iterable<President> getAllPresidents() {
         return presidentsRepository.findAll();
+    }
+
+    public Optional<President> findPresidentById(Integer id) {
+        return presidentsRepository.findById(id);
+    }
+
+    public void removePresidentById(Integer id) {
+        presidentsRepository.deleteById(id);
     }
 
 }
