1cbff811b45444c61e61b06c1fc4573991bffeda
rstorf25 <61019008+rstorf25@users.noreply.github.com>
2024-02-14 09:38:59 -0600

    implement lombok getter, setter in classes. closes #15, #9.

--- src/main/java/com/rs/uspotus/model/BaseEntity.java
+++ src/main/java/com/rs/uspotus/model/BaseEntity.java
@@ -4,10 +4,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 
 import java.io.Serializable;
 
 
+@Getter
 @MappedSuperclass
 public abstract class BaseEntity implements Serializable {
 
@@ -15,7 +17,4 @@ public abstract class BaseEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
 }
