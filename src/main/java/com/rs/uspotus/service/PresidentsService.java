4c1c1b5f4bd126d942c63d6a7405ebbd78042c93
rstorf25 <61019008+rstorf25@users.noreply.github.com>
2024-02-15 16:38:56 -0600

    move business logic out of controller into service layer.closes #21

--- src/main/java/com/rs/uspotus/service/PresidentsService.java
+++ src/main/java/com/rs/uspotus/service/PresidentsService.java
@@ -2,6 +2,7 @@ package com.rs.uspotus.service;
 
 import com.rs.uspotus.model.President;
 import com.rs.uspotus.repository.PresidentsRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -20,8 +21,15 @@ public class PresidentsService {
         return presidentsRepository.save(president);
     }
 
-    public President updatePresident(President president) {
-        return presidentsRepository.save(president);
+    public ResponseEntity<President> updatePresident(Long id, President president) {
+        Optional<President> presOptional = presidentsRepository.findById(id);
+
+        if (presOptional.isPresent()) {
+            return ResponseEntity.ok(presidentsRepository.save(president));
+
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     public Iterable<President> getAllPresidents() {
